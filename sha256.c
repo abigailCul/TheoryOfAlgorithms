67bc6df4c1021f4ae56f92d50b8ac566df3605b4
abigailCul <G00334292@gmit.ie>
2019-02-27 19:46:46 +0000

    up to k constatnts

--- sha256.c
+++ sha256.c
@@ -17,6 +17,13 @@ uint32_t sig1(uint32_t x);
 uint32_t rotr(uint32_t n, uint32_t x);
 uint32_t shr(uint32_t n, uint32_t x);
 
+//4.1.2 definitions
+uint32_t SIG0(uint32_t x);
+uint32_t SIG1(uint32_t x);
+
+uint32_t ch(uint32_t x, uint32_t y, uint32_t z);
+uint32_t Maj(uint32_t x, uint32_t y, uint32_t z);
+
 
 
 int main(int argc, char *argv[]){
@@ -28,6 +35,26 @@ int main(int argc, char *argv[]){
 // return back the 256 bit message digest
 // an array of 32 bit integers
 void sha256(){
+
+	// The k  onstants
+	uint32_t K[] = {
+		0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5,
+		0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
+		0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3, 
+		0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174,
+		0xe49b69c1, 0xefbe4786, 0x0fc19dc6, 0x240ca1cc, 
+		0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da,
+		0x983e5152, 0xa831c66d, 0xb00327c8, 0xbf597fc7, 
+		0xc6e00bf3, 0xd5a79147, 0x06ca6351, 0x14292967,
+		0x27b70a85, 0x2e1b2138, 0x4d2c6dfc, 0x53380d13, 
+		0x650a7354, 0x766a0abb, 0x81c2c92e, 0x92722c85,
+		0xa2bfe8a1, 0xa81a664b, 0xc24b8b70, 0xc76c51a3, 
+		0xd192e819, 0xd6990624, 0xf40e3585, 0x106aa070,
+		0x19a4c116, 0x1e376c08, 0x2748774c, 0x34b0bcb5, 
+		0x391c0cb3, 0x4ed8aa4a, 0x5b9cca4f, 0x682e6ff3,
+		0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208, 
+		0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2
+	};
 
 	// message schedule (6.2)
 	uint32_t W[64];
@@ -51,7 +78,7 @@ void sha256(){
 	};
 
 	//The current message block
-	uint32_t M[16];
+	uint32_t M[16] = {0, 0, 0, 0, 0, 0, 0, 0};
 
 	//w .. 64 element array of 32 bit unsigned integers
 	//m .. 16 element array of 16 bit unsigned integers
@@ -73,8 +100,8 @@ void sha256(){
 
 	//step 3
 	for (t = 0; t < 64; t++) {
-		T1 = h + SIG_1(e) + Ch(e,f,g)+ K[t] + W[t];
-		T2 = SIG_0(a) + Maj(a,b,c);
+		T1 = h + SIG1(e) + Ch(e,f,g)+ K[t] + W[t];
+		T2 = SIG0(a) + Maj(a,b,c);
 		h = g;
 		g = f;
 		f = e;
@@ -95,7 +122,7 @@ void sha256(){
 	H[6] = g + H[6];
 	H[7] = h + H[7];
 
-
+	printf("%x %x %x %x %x %x %x %x\n", H[0], H[1], H[2], H[3], H[4], H[5], H[6], H[7]);
 
 }
 
@@ -118,5 +145,22 @@ uint32_t sig0(uint32_t x){
 uint32_t sig1(uint32_t x){
   // 3.2 4.1.2
   return (rotr(17,x) ^ rotr(19,x) ^ shr(10, x));
+}
+
+
+uint32_t SIG0(uint32_t x){
+	return (rotr(2, x)^ rotr(13, x) ^ rotr(22, x)); 
+}
+
+uint32_t SIG1(uint32_t x){
+	return (rotr(6, x)^ rotr(11, x) ^ rotr(25, x)); 
+}
+
+uint32_t ch(uint32_t x, uint32_t y, uint32_t z){
+	return ((x & y) ^ ((!x) & z));
+}
+
+uint32_t Maj(uint32_t x, uint32_t y, uint32_t z){
+	return ((x & y) ^ (x & z) ^ (y & x));
 }
 
