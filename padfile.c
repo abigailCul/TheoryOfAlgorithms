3235cc6a7d0aec0fe9ff7f020736e435ab855ea4
abigailCul <G00334292@gmit.ie>
2019-03-15 13:28:40 +0000

    printing message 55 bytes

--- padfile.c
+++ padfile.c
@@ -11,6 +11,8 @@ int main(int argc, char *argv[]){
 
 	union msgblock M;
 
+	uint64_t nobits = 0;
+
 	uint64_t nobytes;
 	
 	FILE* f;
@@ -22,7 +24,17 @@ int main(int argc, char *argv[]){
 	while (!feof(f)){
 
 		nobytes = fread(M.e, 1, 64, f);
-		printf("%llu\n", nobytes);
+		nobits = nobits + (nobytes * 8);
+		if(nobytes < 56){
+			printf("I've found a block with less than 55 bytes!\n");
+			M.e[nobytes] = 0x80;
+			while(nobytes <56){
+				nobytes = nobytes +1;
+				M.e[nobytes] = 0x00;
+			}
+			M.s[7] = nobits;
+		}
+		//printf("%llu\n", nobytes);
 	}
 
 
@@ -30,6 +42,11 @@ int main(int argc, char *argv[]){
 	//printf("%c\n", fread(&c, 1, 1, f));
 
 	fclose(f);
+
+	for(int i = 0; i < 64; i++)
+		//print all elemts of m as 64 individual bytes
+		printf("%x", M.e[i]);
+	printf("\n");
 
 	return 0;
 
