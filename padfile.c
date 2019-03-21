8af10377923ec9e8672c5f7d0add4dae0e37416a
abigailCul <G00334292@gmit.ie>
2019-03-21 12:34:22 +0000

    reading bytes

--- padfile.c
+++ padfile.c
@@ -7,6 +7,8 @@ union msgblock{
 	uint64_t s[8];
 };
 
+enum status {READ, PAD0, PAD1, FINISH};
+
 int main(int argc, char *argv[]){
 
 	union msgblock M;
@@ -14,6 +16,8 @@ int main(int argc, char *argv[]){
 	uint64_t nobits = 0;
 
 	uint64_t nobytes;
+
+	enum status S = READ;
 	
 	FILE* f;
 
@@ -21,9 +25,11 @@ int main(int argc, char *argv[]){
 
 	f = fopen(argv[1], "r");
 	
-	while (!feof(f)){
+	while (S == READ){
+
 
 		nobytes = fread(M.e, 1, 64, f);
+		printf("Read %2llu bytes\n", nobytes);
 		nobits = nobits + (nobytes * 8);
 		if(nobytes < 56){
 			printf("I've found a block with less than 55 bytes!\n");
@@ -33,20 +39,37 @@ int main(int argc, char *argv[]){
 				M.e[nobytes] = 0x00;
 			}
 			M.s[7] = nobits;
+			S = FINISH;
+		}	else if(nobytes < 64){
+				S = PAD0;
+				M.e[nobytes] = 0x80;
+				while(nobytes < 64){
+					nobytes = nobytes +1;
+					M.e[nobytes] = 0x00;
+				}
+			}else if(feof(f)){
+				S = PAD1;
+			}
 		}
-		//printf("%llu\n", nobytes);
+
+	if (S == PAD0 || S == PAD1){
+		for (int i = 0; i < 56; i++)
+			M.e[i] = 0x00;
+		M.s[7] = nobits;
 	}
+	if ( S == PAD1)
+		M.e[0] = 0x80;
 
 
-
+		//printf("%llu\n", nobytes);
 	//printf("%c\n", fread(&c, 1, 1, f));
 
 	fclose(f);
 
 	for(int i = 0; i < 64; i++)
 		//print all elemts of m as 64 individual bytes
-		printf("%x", M.e[i]);
-	printf("\n");
+		printf("%x ", M.e[i]);
+		printf("\n");
 
 	return 0;
 
